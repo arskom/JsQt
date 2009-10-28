52db005bde4fc4749272d3e98d471cd5e6a84453
Burak Arslan <burak-github@arskom.com.tr>
2009-10-28 23:36:11 +0000

    no notable changes

--- src/jsuic.py
+++ src/jsuic.py
@@ -2,7 +2,7 @@
 # encoding: utf8
 
 #
-# This file is part of JsQT.
+# This file is part of JsQt.
 #
 # Copyright (C) 2009 Arskom Ltd. www.arskom.com.tr
 #
@@ -18,7 +18,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 #
 
 import sys
